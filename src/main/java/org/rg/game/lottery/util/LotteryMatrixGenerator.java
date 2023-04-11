964f63836944c0e05086e5f935954378073d6065
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-04-11 16:36:08 +0200

    Update

--- src/main/java/org/rg/game/lottery/util/LotteryMatrixGenerator.java
+++ src/main/java/org/rg/game/lottery/util/LotteryMatrixGenerator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
 import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
@@ -47,21 +48,29 @@ public class LotteryMatrixGenerator {
 				return absolutePath.contains(configFilePrefix + "-matrix-generator");
 			})
 		);
+		List<Properties> configurations = new ArrayList<>();
 		for (FileSystemItem fIS : configurationFiles) {
 			try (InputStream configIS = fIS.toInputStream()) {
 				Properties config = new Properties();
 				config.load(configIS);
 				if (Boolean.parseBoolean(config.getProperty("enabled", "false"))) {
-					System.out.println("Processing file '" + fIS.getName() + "' located in '" + fIS.getParent().getAbsolutePath() + "'");
-					LotteryMatrixGeneratorAbstEngine engine = engineSupplier.get();
-					config.setProperty("nameSuffix", fIS.getName().replace("." + fIS.getExtension(), ""));
-					engine.setup(config);
-					if (Boolean.parseBoolean(config.getProperty("async", "false"))) {
-						futures.add(CompletableFuture.runAsync(() -> engine.getExecutor().run()));
-					} else {
-						engine.getExecutor().run();
-					}
+					config.setProperty("file.name", fIS.getName());
+					config.setProperty("file.parent.absolutePath", fIS.getParent().getAbsolutePath());
+					config.setProperty("file.extension", fIS.getExtension());
+					configurations.add(config);
+
 				}
+			}
+		}
+		for (Properties configuration : configurations) {
+			System.out.println("Processing file '" + configuration.getProperty("file.name") + "' located in '" + configuration.getProperty("file.parent.absolutePath") + "'");
+			LotteryMatrixGeneratorAbstEngine engine = engineSupplier.get();
+			configuration.setProperty("nameSuffix", configuration.getProperty("file.name").replace("." + configuration.getProperty("file.extension"), ""));
+			engine.setup(configuration);
+			if (Boolean.parseBoolean(configuration.getProperty("async", "false"))) {
+				futures.add(CompletableFuture.runAsync(() -> engine.getExecutor().run()));
+			} else {
+				engine.getExecutor().run();
 			}
 		}
 	}
