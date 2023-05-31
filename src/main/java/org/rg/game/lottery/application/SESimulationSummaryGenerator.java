73841bd88ba6135942405bb01e83fa40279280a0
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-05-31 17:08:32 +0200

    Update

--- src/main/java/org/rg/game/lottery/application/SESimulationSummaryGenerator.java
+++ src/main/java/org/rg/game/lottery/application/SESimulationSummaryGenerator.java
@@ -6,10 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.poi.common.usermodel.HyperlinkType;
 import org.apache.poi.ss.formula.BaseFormulaEvaluator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -32,7 +34,7 @@ public class SESimulationSummaryGenerator {
 			).findFirst().orElseGet(() -> null);
 		String simulationSummaryFile = simulationSummaryFolder + File.separator + "Summary.xlsx";
 		SimpleWorkbookTemplate workBookTemplate = new SimpleWorkbookTemplate(true);
-		Sheet summarySheet = workBookTemplate.getOrCreateSheet("Risultati", true);
+		Sheet summarySheet = workBookTemplate.getOrCreateSheet("Riepilogo", true);
 		List<String> headerLabels = new ArrayList<>(SELotterySimpleSimulator.excelHeaderLabels);
 		List<String> headersToBeSkipped = Arrays.asList(
 			SELotterySimpleSimulator.DATA_AGGIORNAMENTO_STORICO_LABEL,
@@ -63,11 +65,18 @@ public class SESimulationSummaryGenerator {
 							}
 						}
 					}
+					Cell cellName = workBookTemplate.addCell(singleSimFolder.getName() + "/" + report.getName()).get(0);
+					workBookTemplate.setLinkForCell(
+						HyperlinkType.FILE,
+						cellName,
+						URLEncoder.encode(cellName.getStringCellValue(), "UTF-8")
+					);
 				} catch (Throwable exc) {
 					LogUtils.error("Unable to open file " + report.getAbsolutePath() + ": " + exc.getMessage());
 				}
 			}
 		}
+		summarySheet.setColumnWidth(Shared.getCellIndex(summarySheet, SELotterySimpleSimulator.FILE_LABEL), 12000);
 		try (OutputStream destFileOutputStream = new FileOutputStream(simulationSummaryFile)){
 			workBookTemplate.setAutoFilter(0, reportCounter, 0, headerLabels.size() - 1);
 			BaseFormulaEvaluator.evaluateAllFormulaCells(workBookTemplate.getWorkbook());
