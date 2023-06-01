c5d62b98b39e9aa8cdcd281b538314623c4fbdd8
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-06-01 14:52:16 +0200

    Update

--- scripts/launch-summary-generator-loop.cmd
+++ scripts/launch-summary-generator-loop.cmd
@@ -21,6 +21,6 @@ set working-path.simulations.folder=Simulazioni
 :startLoop
 call "%JAVA_HOME%\bin\java.exe" -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SESimulationSummaryGenerator
 echo:
-echo Waiting 5 minutes before the next update
+echo Waiting 15 minutes before the next update
 timeout /t 900 /NOBREAK > NUL
 goto startLoop
