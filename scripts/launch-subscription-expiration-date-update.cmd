65b45c74d4b99a0f704d39855a0c95a8fff7ce9f
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-06-27 19:38:20 +0200

    Changed JDK version

--- scripts/launch-subscription-expiration-date-update.cmd
+++ scripts/launch-subscription-expiration-date-update.cmd
@@ -6,7 +6,7 @@ if "%CURRENT_DIR:~-1%" == "\" set "CURRENT_DIR_NAME_TEMP=%CURRENT_DIR:~0,-1%"
 for %%f in ("%CURRENT_DIR_NAME_TEMP%") do set "CURRENT_DIR_NAME=%%~nxf"
 
 set CURRENT_UNIT=%CURRENT_DIR:~0,2%
-set JAVA_HOME=%CURRENT_DIR%jdk\19
+set JAVA_HOME=%CURRENT_DIR%jdk\20.0.1
 set lottery-util.working-path=%CURRENT_DIR%..
 set classPath="%CURRENT_DIR%bin"
 
