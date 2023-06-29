5c4db56d43c0a79bf6965851b2ebf211b809293b
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-06-29 05:27:36 +0200

    Update

--- scripts/launch-summary-generator-loop.cmd
+++ scripts/launch-summary-generator-loop.cmd
@@ -1,20 +1,6 @@
 @echo off
-set CURRENT_DIR=%~dp0
-if "%CURRENT_DIR:~-1%" == "\" set "CURRENT_DIR_NAME_TEMP=%CURRENT_DIR:~0,-1%"
 
-for %%f in ("%CURRENT_DIR_NAME_TEMP%") do set "CURRENT_DIR_NAME=%%~nxf"
-
-set CURRENT_UNIT=%CURRENT_DIR:~0,2%
-set JAVA_HOME=%CURRENT_DIR%jdk\20.0.1
-set lottery-util.working-path=%CURRENT_DIR%..
-set classPath="%CURRENT_DIR%bin"
-
-setLocal EnableDelayedExpansion
-set LIBS="
-for /R "%CURRENT_DIR%lib" %%a in (*.jar) do (
-	set LIBS=!LIBS!;%%a
-)
-set LIBS=!LIBS!"
+call "%~dp0set-env.cmd"
 
 set working-path.simulations.folder=Simulazioni
 
--- scripts/simulator-launcher.cmd
+++ scripts/simulator-launcher.cmd
@@ -1,23 +1,10 @@
-set CURRENT_DIR=%~dp0
-if "%CURRENT_DIR:~-1%" == "\" set "CURRENT_DIR_NAME_TEMP=%CURRENT_DIR:~0,-1%"
-
-for %%f in ("%CURRENT_DIR_NAME_TEMP%") do set "CURRENT_DIR_NAME=%%~nxf"
-
-set CURRENT_UNIT=%CURRENT_DIR:~0,2%
-set JAVA_HOME=%CURRENT_DIR%jdk\20.0.1
-set lottery-util.working-path=%CURRENT_DIR%..
-set classPath="%CURRENT_DIR%bin"
-
-setLocal EnableDelayedExpansion
-set LIBS="
-for /R "%CURRENT_DIR%lib" %%a in (*.jar) do (
-	set LIBS=!LIBS!;%%a
-)
-set LIBS=!LIBS!"
+call "%~dp0set-env.cmd"
 
 set working-path.simulations.folder=%CURRENT_DIR_NAME%\config\simulations
 set working-path.complex-simulations.folder=%CURRENT_DIR_NAME%\config\simulations
 ::set tasks.max-parallel=10
 
-start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotterySimpleSimulator
-start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotteryComplexSimulator
+start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx5g -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotterySimpleSimulator
+start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx5g -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotteryComplexSimulator
+
+pause
