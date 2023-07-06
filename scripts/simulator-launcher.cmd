10c197604a4e3f025b558dbed28f8770c61de538
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-07-06 09:22:33 +0200

    Update

--- scripts/simulator-launcher.cmd
+++ scripts/simulator-launcher.cmd
@@ -4,5 +4,18 @@ set working-path.simulations.folder=%CURRENT_DIR_NAME%\config\simulations
 set working-path.complex-simulations.folder=%CURRENT_DIR_NAME%\config\simulations
 ::set tasks.max-parallel=10
 
-start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotterySimpleSimulator
-start "" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotteryComplexSimulator
+
+if [%forceMaster%]==[true] (
+	set firstWindowTile=SE Lottery simple simulator ^(master mode^)
+	set secondWindowTile=SE Lottery complex simulator ^(master mode^)
+) else (
+	set firstWindowTile=SE Lottery simple simulator ^(slave mode^)
+	set secondWindowTile=SE Lottery complex simulator ^(slave mode^)
+)
+
+start "%firstWindowTile%" /D "%~dp0" "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotterySimpleSimulator
+start "%secondWindowTile%" /D "%~dp0" "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotteryComplexSimulator
+
+::I comandi sottostanti anzichè aprire più finestre eseguono tutto nella finestra corrente
+::start "%firstWindowTile%" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotterySimpleSimulator
+::start "%secondWindowTile%" /D "%~dp0" /b "%JAVA_HOME%\bin\java.exe" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SELotteryComplexSimulator
