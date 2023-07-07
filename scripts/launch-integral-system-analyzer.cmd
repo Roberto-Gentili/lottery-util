f3ddcab236d3e41555ec35f3f8e4749502a8436b
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-07-07 17:44:04 +0200

    Update

--- scripts/launch-integral-system-analyzer.cmd
+++ scripts/launch-integral-system-analyzer.cmd
@@ -7,6 +7,8 @@ set working-path.integral-system-analysis.folder=Software\config\integralSystems
 
 if [%logger.type%] == [window] (
 	set lottery.application.name=SE Lottery integral systems analyzer
+	set logger.window.bar.background-color=229,61,48
+	set logger.window.bar.text-color=253,195,17
 	start "%secondWindowTile%" /D "%~dp0" "%JAVA_HOME%\bin\%JAVA_COMMAND%" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SEIntegralSystemAnalyzer
 ) else (
 	call "%JAVA_HOME%\bin\%JAVA_COMMAND%" -Xmx%XMX% -cp %classPath%;%LIBS%;"%CURRENT_DIR%binaries.jar"; org.rg.game.lottery.application.SEIntegralSystemAnalyzer
