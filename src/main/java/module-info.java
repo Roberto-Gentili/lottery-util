4155554f2555cc6fb9984fff3004e4c3d59f69a5
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-07-19 15:47:37 +0200

    Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module org.rg.game.lottery {
 
-    requires jdk.unsupported;
-	requires org.apache.poi.ooxml;
 	requires com.fasterxml.jackson.databind;
-	requires org.jsoup;
-	requires org.apache.poi.poi;
-	requires org.apache.xmlbeans;
+	requires com.formdev.flatlaf;
 	requires java.desktop;
 	requires java.logging;
 	requires javafx.graphics;
-	requires com.formdev.flatlaf;
+    requires jdk.unsupported;
+	requires org.apache.poi.ooxml;
+	requires org.apache.poi.poi;
+	requires org.apache.xmlbeans;
+	requires org.jsoup;
 
 	opens org.rg.game.lottery.application to com.fasterxml.jackson.databind;
 
