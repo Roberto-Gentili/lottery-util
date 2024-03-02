e31fd5ef7b8dda354bf1fde5712af44b8e2644ec
Roberto Gentili <roberto.gentili.1980@gmail.com>
2024-03-02 20:19:49 +0100

    Update

--- scripts/set-env.cmd
+++ scripts/set-env.cmd
@@ -4,7 +4,7 @@ if "%CURRENT_DIR:~-1%" == "\" set "CURRENT_DIR_NAME_TEMP=%CURRENT_DIR:~0,-1%"
 for %%f in ("%CURRENT_DIR_NAME_TEMP%") do set "CURRENT_DIR_NAME=%%~nxf"
 
 set CURRENT_UNIT=%CURRENT_DIR:~0,2%
-set JAVA_HOME=%CURRENT_DIR%jdk\20.0.1
+set JAVA_HOME=%CURRENT_DIR%jdk\21.0.2-GraalVM
 set lottery-util.working-path=%CURRENT_DIR%..
 set classPath="%CURRENT_DIR%bin"
 
