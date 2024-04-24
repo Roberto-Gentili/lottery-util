410c23c3e8a6445c806c0b84eee2354bb950205a
Roberto Gentili <roberto.gentili.1980@gmail.com>
2024-04-24 16:50:24 +0200

    Update

---
+++ run-integral-system-analysis.sh
@@ -0,0 +1,8 @@
+#!bin/bash/
+export FIREBASE_CREDENTIALS_FILE='/home/dale/lottery-util-firebase-credentials.json'
+export FIREBASE_URL='https://lottery-util-dd398-default-rtdb.europe-west1.firebasedatabase.app'
+export ASYNC='false'
+export AUTOSAVE_EVERY='3000000'
+export LOGGER_SHOW_THREAD_INFO='true'
+export TASKS_MAX_PARALLEL='8'
+mvn -B compile exec:java -P integral-system-analysis --file pom.xml
