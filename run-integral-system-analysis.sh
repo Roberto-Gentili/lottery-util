0b854de7725bc22d0b443eb981a74dd43e4a3083
Roberto Gentili <roberto.gentili.1980@gmail.com>
2024-04-25 08:39:15 +0200

    Improved functionalities

--- run-integral-system-analysis.sh
+++ run-integral-system-analysis.sh
@@ -2,7 +2,7 @@
 export FIREBASE_CREDENTIALS_FILE='../lottery-util-firebase-credentials.json'
 export FIREBASE_URL='https://lottery-util-dd398-default-rtdb.europe-west1.firebasedatabase.app'
 export LOTTERY_UTIL_WORKING_PATH='../lottery-util-workspace'
-export INTEGRAL_SYSTEM_ANALYSIS_FOLDER='/home/dale/lottery-util/target/classes/integralSystemsAnalysis'
+export INTEGRAL_SYSTEM_ANALYSIS_FOLDER='../lottery-util/target/classes/integralSystemsAnalysis'
 export ASYNC='false'
 export AUTOSAVE_EVERY='3000000'
 export LOGGER_SHOW_THREAD_INFO='true'
