c8c904afe685a1c4e0284281dad78d4ef95a1b4d
Roberto Gentili <roberto.gentili.1980@gmail.com>
2024-04-25 08:31:09 +0200

    Update

--- run-integral-system-analysis.sh
+++ run-integral-system-analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export FIREBASE_CREDENTIALS_FILE='./../lottery-util-firebase-credentials.json'
+export FIREBASE_CREDENTIALS_FILE='../lottery-util-firebase-credentials.json'
 export FIREBASE_URL='https://lottery-util-dd398-default-rtdb.europe-west1.firebasedatabase.app'
-export LOTTERY_UTIL_WORKING_PATH='/home/dale/lottery-util-workspace'
+export LOTTERY_UTIL_WORKING_PATH='../lottery-util-workspace'
 export INTEGRAL_SYSTEM_ANALYSIS_FOLDER='/home/dale/lottery-util/target/classes/integralSystemsAnalysis'
 export ASYNC='false'
 export AUTOSAVE_EVERY='3000000'
