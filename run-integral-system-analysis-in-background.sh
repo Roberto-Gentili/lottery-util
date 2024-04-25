4533d3782ab33163753fe81568930e35cb2f67d8
Roberto Gentili <roberto.gentili.1980@gmail.com>
2024-04-25 09:37:20 +0200

    Update

--- run-integral-system-analysis-in-background.sh
+++ run-integral-system-analysis-in-background.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for (( c=0; c<3; c++ ))
+for (( c=0; c<3; c++ ));
 do 
 	#Run in detached mode
-	screen -d -m ./run-integral-system-analysis.sh
-done
+	screen -d -m ./run-integral-system-analysis.sh;
+done;
 #List all background sessions
-screen -ls
+screen -ls;
 #To kill a detached session: screen -X -S ${processId} quit
 #To attach to a background session: screen -r ${processId}
 #To detach from process: CTRL+A+D
