51c8238bee30af5740ddfeb5a125c1521409c283
dale <user.name>
2024-04-25 06:25:58 +0000

    Update

--- run-integral-system-analysis.sh
+++ run-integral-system-analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export FIREBASE_CREDENTIALS_FILE='/home/dale/lottery-util-firebase-credentials.json'
+export FIREBASE_CREDENTIALS_FILE='./../lottery-util-firebase-credentials.json'
 export FIREBASE_URL='https://lottery-util-dd398-default-rtdb.europe-west1.firebasedatabase.app'
 export LOTTERY_UTIL_WORKING_PATH='/home/dale/lottery-util-workspace'
 export INTEGRAL_SYSTEM_ANALYSIS_FOLDER='/home/dale/lottery-util/target/classes/integralSystemsAnalysis'
